ba0bf438e4cb900332f1ab2d862ab004578b519e
sofiabruno <55451343+sofiabruno@users.noreply.github.com>
2020-02-20 17:50:16 -0300

    tiene lo del localstorage pero hay que verlo mejor

--- functions.js
+++ functions.js
@@ -31,6 +31,8 @@ ons.ready(function () {
         $("#ojitoSi").hide();
         var ema = $("#l_email").val();
         var pwd = $("#l_password").val();
+
+        var recordar = document.getElementById("recordar");
         
         try {
             if (!ema) {
@@ -53,6 +55,14 @@ ons.ready(function () {
                 success: function (respuesta) {
 
                     var usuario = respuesta;
+
+                    // ejemplo del ejercicio q mando pero ni idea como seguir dsp en el otro
+                    if (recordar.checked) {
+                        localStorage.setItem("usuario", usuario);
+                    } else {
+                        sessionStorage.setItem("usuario", usuario);
+                    }    
+
                     sessionStorage.setItem("usr", usuario);
                     var nav = document.getElementById("nav");
                     nav.pushPage("t_productos");
@@ -101,48 +111,48 @@ ons.ready(function () {
 
     });
 
-    $("#btn_registrarme").on("click", function () {
-        var ema = $("#email").val();
-        var pwd = $("#password").val();
+    // $("#btn_registrarme").on("click", function () {
+    //     var ema = $("#email").val();
+    //     var pwd = $("#password").val();
 
-        try {
-            if (!ema) {
-                throw "Ingrese email para continuar";
-            }
-            if (!pwd) {
-                throw "Ingrese contrase&ntilde;a para continuar";
-            }
+    //     try {
+    //         if (!ema) {
+    //             throw "Ingrese email para continuar";
+    //         }
+    //         if (!pwd) {
+    //             throw "Ingrese contrase&ntilde;a para continuar";
+    //         }
 
-            // Invocar API POST usuario
-            datos = { "email": ema, "password": pwd };
+    //         // Invocar API POST usuario
+    //         datos = { "email": ema, "password": pwd };
 
-            $.ajax({
-                url: "https://tiendanatural2020.herokuapp.com/api/user/register",
-                type: "POST",
-                dateType: "json",
-                data: datos,
-                success: function (respuesta) {
-                    ons.notification.toast("El usuario se registro correctamente", { "timeout": 3000 });
+    //         $.ajax({
+    //             url: "https://tiendanatural2020.herokuapp.com/api/user/register",
+    //             type: "POST",
+    //             dateType: "json",
+    //             data: datos,
+    //             success: function (respuesta) {
+    //                 ons.notification.toast("El usuario se registro correctamente", { "timeout": 3000 });
 
-                    var usuario = respuesta;
-                    sessionStorage.setItem("usr", usuario);
-                    var nav = document.getElementById("nav");
-                    nav.pushPage("t_productos");
-                },
+    //                 var usuario = respuesta;
+    //                 sessionStorage.setItem("usr", usuario);
+    //                 var nav = document.getElementById("nav");
+    //                 nav.pushPage("t_productos");
+    //             },
 
-                error: function (xml, err, status) {
-                    if (xml.responseJSON.data) {
-                        ons.notification.toast(xml.responseJSON.data.error.errors.email.message, { "timeout": 3000 });
-                    } else {
-                        ons.notification.toast(xml.responseJSON.reason, { "timeout": 3000 });
-                    }
-                }
-            });
+    //             error: function (xml, err, status) {
+    //                 if (xml.responseJSON.data) {
+    //                     ons.notification.toast(xml.responseJSON.data.error.errors.email.message, { "timeout": 3000 });
+    //                 } else {
+    //                     ons.notification.toast(xml.responseJSON.reason, { "timeout": 3000 });
+    //                 }
+    //             }
+    //         });
 
-        } catch (e) {
-            ons.notification.toast(e, { "timeout": 3000 });
-        }
-    }); 
+    //     } catch (e) {
+    //         ons.notification.toast(e, { "timeout": 3000 });
+    //     }
+    // }); 
 
 
 
